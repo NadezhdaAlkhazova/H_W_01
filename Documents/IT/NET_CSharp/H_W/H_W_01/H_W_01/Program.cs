9fc41d514741c670e4080257aacde80392a95647
NadezhdaAlkhazova <shailer5407@gmail.com>
2025-06-29 19:42:48 +0300

    add

---
+++ Documents/IT/NET_CSharp/H_W/H_W_01/H_W_01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace H_W_01;
+
+class Program {
+    static void Main() {
+        Task1.Run();
+        Task2.Run();
+        Task3.Run();
+        Task4.Run();
+        Task5.Run();
+        Task6.Run();
+        Task7.Run();
+    }
+}
